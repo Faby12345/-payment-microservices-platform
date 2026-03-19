da058a5b85da527d623566c84939ea468e5abd91
Turle Fabian <fabyturlw@gmail.com>
2026-03-19 16:35:59 +0200

    feat(auth): added logging + small impreve for roles(remove creating a Set<> and just updated the existing one)

--- backend/auth-service/src/main/java/app/authservice/service/AuthService.java
+++ backend/auth-service/src/main/java/app/authservice/service/AuthService.java
@@ -9,6 +9,7 @@ import app.authservice.web.dto.request.UserRegisterRequestDto;
 import app.authservice.web.dto.response.UserResponseDto;
 import app.authservice.web.exception.EmailAlreadyExistsException;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class AuthService {
     private final UserRepository userRepository;
@@ -35,33 +37,39 @@ public class AuthService {
     }
 
 
+
     @Transactional
     public UserResponseDto register(UserRegisterRequestDto dto){
+        log.info("Attempting to register new user with email: {}", dto.email());
 
         if (userRepository.existsByEmail(dto.email())) {
+            log.warn("Registration failed: Email {} is already taken", dto.email());
             throw new EmailAlreadyExistsException("Email is already taken");
         }
 
         String passwordHash = passwordEncoder.encode(dto.password());
         Role role = roleService.findRoleByName("USER");
+
         /**
          * When we add a role for user, it will automatically insert into
          * user_role table a new role for the user (CHECK USER ENTITY CLASS
          * - the roles field annotaion)
          */
 
-        Set<Role> userRoles = new HashSet<>();
-        userRoles.add(role);
         User newUser = userMapper.toEntity(dto, passwordHash);
-        newUser.setRoles(userRoles);
+        newUser.getRoles().add(role);
 
 
         try{
             User savedUser = userRepository.save(newUser);
+            log.info("feat(auth-service-REGISTER ): User with id: {}, name: {} created SUCCESFULLY",
+                    newUser.getId(),
+                    newUser.getFirstName() + " " + newUser.getLastName());
             return userMapper.toResponse(savedUser);
 
         } catch (DataIntegrityViolationException e) {
             // Safe-guard against race conditions
+            log.error("Critical Race Condition: Duplicate email detected during DB insert for {}", dto.email());
             throw new EmailAlreadyExistsException("Email is already registered");
         }
 
