a0f246abdcf83f5720b90b6da50378ca9867c7cc
Turle Fabian <fabyturlw@gmail.com>
2026-04-24 12:44:04 +0300

    added @Repository annotation (good practice)

--- backend/wallet-service/src/main/java/app/walletservice/repository/TransactionRepository.java
+++ backend/wallet-service/src/main/java/app/walletservice/repository/TransactionRepository.java
@@ -1,11 +1,14 @@
 package app.walletservice.repository;
 
 import app.walletservice.entity.Transaction;
+import jdk.jfr.Registered;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     Optional<Transaction> findByIdempotencyKey(String idempotencyKey);
     Optional<Transaction> findByReference(String reference);
